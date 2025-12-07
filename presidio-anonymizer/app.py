c44dc99f56edaa067e441c78f4fff5ef7173122f
Yasrib Yasir <yfarook@emich.edu>
2025-12-07 21:32:45 +0000

    Fix ruff import sorting and unused imports

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -6,15 +6,10 @@ from logging.config import fileConfig
 from pathlib import Path
 
 from flask import Flask, Response, jsonify, request
-from presidio_anonymizer import (
-    AnonymizerEngine,
-    DeanonymizeEngine,
-    InvalidParamError,
-    RecognizerResult,
-    OperatorConfig,
-)
-from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 from werkzeug.exceptions import BadRequest, HTTPException
+from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
+from presidio_anonymizer.entities import InvalidParamError
+from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 
 DEFAULT_PORT = "3000"
 LOGGING_CONF_FILE = "logging.ini"
